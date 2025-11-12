e64b13a43e4895d6bffe4eefe44c1744dde0f9f3
Tadeo Saracho <sarachotadeo@gmail.com>
2025-11-11 22:09:29 -0300

    Agregue el js, prueben comprar en los botones

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementsByClassName("button")[0].addEventListener("click", function() {
+    alert("Has seleccionado el plan semanal de ProtoWeb. ¡Gracias por tu compra!");
+});
+document.getElementsByClassName("button")[1].addEventListener("click", function() {
+    alert("Has seleccionado el plan mensual de ProtoWeb. ¡Gracias por tu compra!");
+}   );  
+document.getElementsByClassName("button")[2].addEventListener("click", function() {
+    alert("Has seleccionado el plan anual de ProtoWeb. ¡Gracias por tu compra!");
+}   );  
+
